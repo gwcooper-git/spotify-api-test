6e2de7c13bde7cf8325ac7936fb73183d97c1bec
Grant Cooper <grantcoope98@gmail.com>
2021-05-28 15:27:56 -0500

    fix uri

--- script.js
+++ script.js
@@ -24,7 +24,7 @@ $( document ).ready(function() {
     let client_id = '9c2a891ca1744574b981ec034c36daea';
     // Use the following site to convert your regular url to the encoded version:
     // https://www.url-encode-decode.com/
-    let redirect_uri = 'https%3A%2F%2Fgwcooper-git.github.io%2Fspotify-api-test%2F'; // GitHub Pages URL or whatever your public url to this app is
+    let redirect_uri = 'https%3A%2F%2Fgwcooper-git.github.io%2Fspotify-api-test'; // GitHub Pages URL or whatever your public url to this app is
     // *************** END *************************
 
     const redirect = `https://accounts.spotify.com/authorize?client_id=${client_id}&response_type=token&redirect_uri=${redirect_uri}`;
