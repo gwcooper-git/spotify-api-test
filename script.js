59c00337f673f93bd2f0ec1c0862d61a9baec26e
gwcooper-git <84594249+gwcooper-git@users.noreply.github.com>
2021-06-17 21:06:37 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,23 +1,23 @@
 // Document has been loaded
-//$( document ).ready(function() {
-     // Helper Function to Extract Access Token for URL
-  //  const getUrlParameter = (sParam) => {
-    //  let sPageURL = window.location.search.substring(1),////substring will take everything after the https link and split the #/&
-      //    sURLVariables = sPageURL != undefined && sPageURL.length > 0 ? sPageURL.split('#') : [],
-        //  sParameterName,
-          //i;
-     // let split_str = window.location.href.length > 0 ? window.location.href.split('#') : [];
-    //  sURLVariables = split_str != undefined && split_str.length > 1 && split_str[1].length > 0 ? split_str[1].split('&') : [];
-      //for (i = 0; i < sURLVariables.length; i++) {
-        //  sParameterName = sURLVariables[i].split('=');
-          //if (sParameterName[0] === sParam) {
-            //  return sParameterName[1] === undefined ? true : decodeURIComponent(sParameterName[1]);
-//          }
-  //    }
- // };
+$( document ).ready(function() {
+      Helper Function to Extract Access Token for URL
+    const getUrlParameter = (sParam) => {
+      let sPageURL = window.location.search.substring(1),////substring will take everything after the https link and split the #/&
+          sURLVariables = sPageURL != undefined && sPageURL.length > 0 ? sPageURL.split('#') : [],
+          sParameterName,
+          i;
+      let split_str = window.location.href.length > 0 ? window.location.href.split('#') : [];
+      sURLVariables = split_str != undefined && split_str.length > 1 && split_str[1].length > 0 ? split_str[1].split('&') : [];
+      for (i = 0; i < sURLVariables.length; i++) {
+          sParameterName = sURLVariables[i].split('=');
+          if (sParameterName[0] === sParam) {
+              return sParameterName[1] === undefined ? true : decodeURIComponent(sParameterName[1]);
+          }
+      }
+  };
 
     // Get Access Token
-    // const accessToken = getUrlParameter('access_token');
+     const accessToken = getUrlParameter('access_token');
  
      
     // AUTHORIZE with Spotify (if needed)
